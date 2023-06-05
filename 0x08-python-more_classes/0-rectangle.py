ef1d9a15206013a51d086666cd966c12a4eea92f
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-06-05 15:57:32 -0700

    Task Completed

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 2020
+@author: Robinson Montes
+"""
+
 
 class Rectangle:
     """Empty class Rectangle that defines a rectangle figure
@@ -9,4 +14,3 @@ class Rectangle:
     """
 
     pass
-
