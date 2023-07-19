d190800ef498a9b019749b6c6dbaff2afb1da1c6
Bright Kusi Appiah <45246560+KUSAPPI@users.noreply.github.com>
2023-07-19 22:50:48 +0000

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,5 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Create a user with a password
+-- DDL query to create a MySQL user with password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- add privileges to user
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
